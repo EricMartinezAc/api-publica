36ecaf9b9622681f7868759c78ff1678777e0b89
Eric Martinez Acelas <emartinezacelas@gmail.com>
2024-04-06 23:00:10 -0500

    middleware json, 404, cors y verifica token

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require("express");
+const cors_ = require("cors");
 const router = require("./src/router");
 const App = express();
 
 App.use(router);
 App.set("port", process.env.PORT || 2024);
+
+// middlewares
+App.use(
+  express.json({
+    limit: "35mb",
+  })
+);
+App.use(
+  cors_({
+    origin: "*",
+  })
+);
+
+//Route 404
+App.use("*", (req, res) => {
+  console.error(`404 from ${req.url} by ${req.ip}`);
+  res.status(404);
+});
 
 App.listen(App.get("port"), () => {
   console.log(`${App.get("port")} is run`);
--- src/router.js
+++ src/router.js
@@ -1,7 +1,29 @@
+const { verify } = require("crypto");
 const express = require("express");
 const router = express.Router();
+
+//MIDDLEWARES
+//-- verifica si viene cabecera
+function VerifyInToken(req, res, next) {
+  const Bheader = req.headers["autorization"];
+  if (typeof Bheader !== "undefined") {
+    req.token = Bheader.split(" ")[1];
+    req.id_prod = Bheader.split(" ")[2];
+    req.user = Bheader.split(" ")[3];
+    console.log(Bheader);
+    next();
+  } else {
+    console.error(403);
+    res.sendStatus(403);
+  }
+}
+
 router.get("/", (req, res) => {
   res.json({ key: "value" });
+});
+
+router.post("/", VerifyInToken, (req, res) => {
+  res.json({ new: "der" });
 });
 
 module.exports = router;
