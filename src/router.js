f8b56d7a1bc7df8f329e3890d65d881d671c46c6
Eric Martinez Acelas <emartinezacelas@gmail.com>
2024-04-06 23:12:16 -0500

    generate token

--- src/router.js
+++ src/router.js
@@ -1,8 +1,33 @@
-const { verify } = require("crypto");
+const jwt = require("jsonwebtoken");
 const express = require("express");
 const router = express.Router();
 
 //MIDDLEWARES
+//- generate token
+function GenerateToken(datos) {
+  jwt.sign(
+    datos.user + ";" + String(new Date(Date.now()).getDate()),
+    "Rouse17*",
+    async (err, token) => {
+      if (err === null) {
+        //informe de respuesta
+        console.log(token);
+        await res.json({
+          valor: 400,
+          msj: `token generado ${token}, ahora tienes el control`,
+        });
+      } else {
+        console.log(
+          `No se pudo generar token para ${token}. Error E-404: ${err}`
+        );
+        res.json({
+          valor: 404,
+          msj: `No se pudo generar token`,
+        });
+      }
+    }
+  );
+}
 //-- verifica si viene cabecera
 function VerifyInToken(req, res, next) {
   const Bheader = req.headers["autorization"];
@@ -22,7 +47,7 @@ router.get("/", (req, res) => {
   res.json({ key: "value" });
 });
 
-router.post("/", VerifyInToken, (req, res) => {
+router.post("/", GenerateToken, (req, res) => {
   res.json({ new: "der" });
 });
 
