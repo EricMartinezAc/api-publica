7422dd562354eda87cfa159034b3db66570c55e8
Eric Martinez Acelas <emartinezacelas@gmail.com>
2024-04-12 12:40:09 -0500

    dbase co

---
+++ src/DBase/Mongoose/ConexionMongo.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+//process.env.mongodb_UR
+const Conexiondb = (id_prod) =>
+  mongoose.connect(
+    `${process.env.MONGODB_URI}${id_prod}${process.env.MONGODB_URI_config}`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  );
+mongoose.set("strictQuery", true);
+
+module.exports = Conexiondb;
