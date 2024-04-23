0e866855081aed5f4a4996ff4216b64c80688121
Eric Martinez Acelas <emartinezacelas@gmail.com>
2024-04-23 10:45:18 -0500

    render is failure

--- src/Routes/routerARControllerWeb/Middlewares/VerifyInToken.js
+++ src/Routes/routerARControllerWeb/Middlewares/VerifyInToken.js
@@ -3,17 +3,17 @@ const jwt = require("jsonwebtoken");
 async function verifyInToken(Bearer, req) {
   try {
     if (typeof Bearer !== "undefined") {
-      // req.token = await Bearer.split(" ")[1];
-      // req.user = await Bearer.split("")[2];
-      // req.owner = await Bearer.split(" ")[3];
-      // console.log([Bearer, req.token, req.user, req.owner]);
-      // const resp = await jwt.verify(
-      //   req.token,
-      //   process.env.PSW_JWT,
-      //   (error, data) => {
-      //     return error ? false : true;
-      //   }
-      // );
+      req.token = await Bearer.split(" ")[1];
+      req.user = await Bearer.split("")[2];
+      req.owner = await Bearer.split(" ")[3];
+      console.log([Bearer, req.token, req.user, req.owner]);
+      const resp = await jwt.verify(
+        req.token,
+        process.env.PSW_JWT,
+        (error, data) => {
+          return error ? false : true;
+        }
+      );
       return await true;
     } else {
       console.error("verifytoken", 403);
